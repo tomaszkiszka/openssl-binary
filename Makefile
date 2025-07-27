af299e1b24534769d481944b354121ff8f6aae11
Tomasz Kiszka <tomasz.kiszka@revolut.com>
2025-07-27 16:35:12 +0200

    OpenSSL3

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-BUILD_VERSION ?= 1.1.2301
+BUILD_VERSION ?= 3.3.3001
 
 distribute:
 	Scripts/make_xcframework.sh build "$(BUILD_VERSION)"
--- scripts/make_xcframework.sh
+++ scripts/make_xcframework.sh
@@ -10,38 +10,11 @@ clone() {
     git clone --branch "${VERSION}" --depth 1 https://github.com/krzyzanowskim/OpenSSL.git "${BUILD_DIR}/${VERSION}"
 }
 
-patch() {
-    readonly VERSION="${1}"
-
-    if [ -d "patches/${VERSION}" ]; then
-        echo "Found patches for ${VERSION}, applying..."
-
-        pushd "${BUILD_DIR}/${VERSION}" > /dev/null
-
-        for patch in ../../patches/${VERSION}/*.patch; do
-            if [ -f "$patch" ]; then
-                echo "Applying patch: $(basename "$patch")"
-                git apply --ignore-whitespace "$patch"
-            fi
-        done
-
-        popd > /dev/null
-        echo "All patches applied for ${VERSION}"
-    fi
-}
-
 build_xcframework() {
     readonly VERSION="${1}"
     readonly OPENSSL_OUTPUT_DIR="${BUILD_DIR}/${VERSION}/Frameworks"
 
-    # Clean the openssl output directory (they have it in source control)
-    rm -rf "${OPENSSL_OUTPUT_DIR}"
-    mkdir -p "${OPENSSL_OUTPUT_DIR}"
-
-    # Create xcframework using the openssl script
-    (cd "${BUILD_DIR}/${VERSION}" && ./scripts/create-frameworks.sh)
-
-    # The generated xcframework contains slices for macos and macosx_catalyst.
+    # The generated xcframework contains slices for macos and macosx_catalyst (and many more).
     # We dont need those, so instead create our own xcframework with just the iphoneos
     # and iphonesimulator slices
     xcrun xcodebuild -quiet -create-xcframework \
@@ -52,7 +25,6 @@ build_xcframework() {
 
 build() {
     clone "$@"
-    patch "$@"
     build_xcframework "$@"
 }
 
